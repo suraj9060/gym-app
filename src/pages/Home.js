0f760ac25100fcbc74f92582c4b12e576764defb
suraj9060 <87435542+suraj9060@users.noreply.github.com>
2022-07-12 23:12:14 +0530

    HeroBanner Page done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Box } from '@mui/system'
+import HeroBannner from '../components/HeroBannner'
+import SearchExercises from '../components/SearchExercises'
+import Exercises from '../components/Exercises'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Box>
+      <HeroBannner />
+      <SearchExercises />
+      <Exercises />
+    </Box>
   )
 }
 
