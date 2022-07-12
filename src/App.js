afe05f7e9184e425457c99c3a37150ded500c048
suraj9060 <87435542+suraj9060@users.noreply.github.com>
2022-07-12 22:33:16 +0530

    nav-bar completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-
 import './App.css';
+import { Box } from "@mui/material"
+import { Routes, Route } from "react-router-dom"
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import ExerciseDetails from './pages/ExerciseDetails';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-     <h1>Hello world</h1>
-    </div>
+    <Box width={"400px"} sx={{width:{xl:"1488px"}}} m="auto">
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetails />} />
+      </Routes>
+      <Footer />
+
+    
+    </Box>
   );
 }
 
