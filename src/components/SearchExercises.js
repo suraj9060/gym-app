f43bc8a6441d047090637f34eca5c09a1808cfd1
suraj9060 <87435542+suraj9060@users.noreply.github.com>
2022-07-13 00:42:47 +0530

    Fethiching data from Rapid api

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Box, Botton, Stack, TextField, Typography, Button } from "@mui/material";
+import { textTransform } from "@mui/system";
 
 const SearchExercises = () => {
+    const [search, setSearch] = useState("")
+    
+    const handleSearch = () => {
+        
+    }
   return (
-    <div>SearchExercises</div>
-  )
-}
+    <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
+      <Typography
+        fontWeight={700}
+        sx={{ fontSize: { lg: "44px", xs: "30px" } }}
+        mb="50px"
+        textAlign="center"
+      >
+        Awesome Exercise You <br /> Should Know
+      </Typography>
+      <Box position="relative" mb="72px">
+        <TextField
+          sx={{
+            input: { fontWeight: "700", border: "none", borderRadius: "40px" },
+            width: { lg: "800px", xs: "350px" },
+            backgroundColor: "#fff",
+            borderRadius: "40px",
+          }}
+          height="76px"
+          value={search }
+          onChange={(e) => {setSearch(e.target.value.toLowerCase())}}
+          placeholder="Search Exercise"
+          type="text"
+              />
+              <Button className="Search-btn"
+                  sx={{
+                      bgcolor: "#FF2625",
+                      color: "#fff",
+                      textTransform: "none",
+                      width: { lg: "175px", xs: "80px" },
+                      fontSize: { lg: "20px", xs: "14px" },
+                      height: "56px",
+                      position: "absolute",
+                      right:0
+                  }}
+                  onClick={handleSearch}
+              >Search</Button>
+      </Box>
+    </Stack>
+  );
+};
 
-export default SearchExercises
+export default SearchExercises;
